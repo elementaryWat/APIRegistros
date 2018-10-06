df72d52c6d6f92054c4a01426e244bda78c5b4bc
Augusto Romero <geragusto@hotmail.com>
2018-10-06 13:20:28 -0300

    EndPOints Editar Familias

--- routes/hermano.js
+++ routes/hermano.js
@@ -5,10 +5,11 @@ const hermanoController=require('../controllers/hermano');
 hermanoRouter.post('/',mwAuthenticated.ensureAuth,hermanoController.agregarHermano);
 hermanoRouter.put('/:hermanoId',mwAuthenticated.ensureAuth,hermanoController.actualizarDatosHermano);
 hermanoRouter.get('/listaFamilias/:congregacion',mwAuthenticated.ensureAuth,hermanoController.obtenerFamilias);
-hermanoRouter.get('/listaHermanos/:familia',mwAuthenticated.ensureAuth,hermanoController.obtenerHermanosFamilia);
+hermanoRouter.post('/listaHermanos/:familia',mwAuthenticated.ensureAuth,hermanoController.obtenerHermanosFamilia);
 hermanoRouter.post('/actualizar',mwAuthenticated.ensureAuth,hermanoController.actualizarDatosHermano);
 hermanoRouter.post('/agregarFamilia',mwAuthenticated.ensureAuth,hermanoController.agregarFamilia);
 hermanoRouter.put('/editarFamilia/:familiaId',mwAuthenticated.ensureAuth,hermanoController.editarFamilia);
 hermanoRouter.post('/existeFamilia',mwAuthenticated.ensureAuth,hermanoController.existeFamilia);
+hermanoRouter.post('/existeIntegranteFamilia',mwAuthenticated.ensureAuth,hermanoController.existeIntegranteEnFamilia);
 
 module.exports=hermanoRouter;   
