cae7e208413b2e8a5d499ff8612832ddd7114596
Augusto Romero <geragusto@hotmail.com>
2018-10-07 20:13:35 -0300

    Nested Find From Collections

--- controllers/informe.js
+++ controllers/informe.js
@@ -1,4 +1,7 @@
 const Informes = require('../models/informe');
+const Hermanos = require('../models/hermano');
+const Familias = require('../models/familia');
+
 
 function agregarInforme(req, res) {
     Informes.create(req.body).then(nuevoInforme => {
@@ -11,16 +14,27 @@ function agregarInforme(req, res) {
 
 function obtenerInformes(req, res) {
     let congregacion = req.params.congregacion;
-    //Devuelve los informes de los hermanos de una congregacion
-    Informes.find({}).populate({path:'hermano',populate:{path:'familia', match:{ congregacion:congregacion}}})
-    .where('hermano.familia').equals(null)
-    .exec()
-        .then(informes => {
-            res.status(200).send({ informes });
+    /* Informes.find({}).populate({path:'hermano',populate:{path:'familia', match:{ congregacion:congregacion}}})
+    .where('hermano.familia').equals(null) */
+    //Devuelve los informes de los hermanos de familias de una congregacion
+    Familias.find({congregacion:congregacion}).exec()
+        .then(familias=>{
+            var idsFamiliasCong=familias.map((familia)=>{return familia._id});
+            Hermanos.find({familia: {$in: idsFamiliasCong}}).exec()
+                .then(hermanosCong=>{
+                    var idsHermanosCong=hermanosCong.map((hermano)=>{return hermano._id});
+                    Informes.find({hermano: {$in: idsHermanosCong}})
+                    .exec()
+                    .then(informes => {
+                        res.status(200).send({ informes }); 
+                    })
+                    .catch(error => {
+                        res.status(500).send({ error, message: "Ocurrio un error al obtener los informes de la congregacion" })
+                    })
+                })
         })
-        .catch(error => {
-            res.status(500).send({ error, message: "Ocurrio un error al obtener los informes de la congregacion" })
-        })
+    
+    
 }
 
 module.exports={
